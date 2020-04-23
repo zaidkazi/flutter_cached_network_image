d8648849aa807bf0809e30db6fd296cfbdc7ba0f
Rene Floor <r.floor.1@gmail.com>
2020-04-23 15:40:27 +0200

    Remove unused files

--- lib/src/widget/error_widget.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ErrorIconWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Icon(Icons.error, color: Colors.red,),);
-  }
-}
